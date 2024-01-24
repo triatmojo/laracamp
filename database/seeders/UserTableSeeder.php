6ff96ff9536d3fff3229fba831b760a190afc503
Tri atmojo <ta.sukmono@scisi.com>
2024-01-24 16:41:17 +0700

    feat: add seeder users and camps

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' =>  Hash::make('admin123'),
+                'occupation' => 'System Engineer',
+                'is_admin' => '1',
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time())
+            ],
+            [
+                'name' => 'user',
+                'email' => 'user@gmail.com',
+                'password' =>  Hash::make('user123'),
+                'occupation' => 'Product Manager',
+                'is_admin' => '0',
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time())
+            ]
+        ];
+    }
+}
