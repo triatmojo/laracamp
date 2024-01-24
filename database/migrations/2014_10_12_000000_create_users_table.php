94bd166aa0e49ac6164c9b15c6400cba068eaf58
Tri atmojo <ta.sukmono@scisi.com>
2024-01-24 10:05:41 +0700

    feat: finish migration users, camps, camp_benefits, checkouts

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use function Laravel\Prompts\table;
+
 return new class extends Migration
 {
     /**
@@ -16,9 +18,14 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('occupation')->nullable();
+            $table->boolean('is_admin')->default(false);
+            
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
