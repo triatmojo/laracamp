932fc636d546d19137fa3a240b760ad2bd562aba
triatmojo <trieatmojo98tata@gmail.com>
2024-04-21 07:29:10 +0700

    feat: delete function invoice

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -99,8 +99,4 @@ class CheckoutController extends Controller
         return view('checkout.success');
     }
 
-    public function invoice(Checkout $checkout)
-    {
-         return $checkout;
-    }
 }
