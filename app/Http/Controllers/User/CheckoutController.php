60ea510d0f101aad658bb8b38d5422a62ba96693
triatmojo <trieatmojo98tata@gmail.com>
2024-05-25 19:46:43 +0700

    feat: init midtrans

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -4,16 +4,29 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Mail;
 use App\Http\Requests\User\Checkout\Store;
+use Midtrans;
+
 use App\Mail\Checkout\AfterCheckout;
 use App\Models\Checkout;
 use App\Models\Camp;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Redis;
-use Illuminate\Support\Facades\Mail;
+
+
 
 class CheckoutController extends Controller
 {
+
+    public function __construct()
+    {
+        Midtrans\Config::$serverKey = env('MIDTRANS_SERVERKEY');
+        Midtrans\Config::$isProduction = env('MIDTRANS_IS_PRODUCTION');
+        Midtrans\Config::$isSanitized = env('MIDTRANS_IS_SANITIZED');
+        Midtrans\Config::$is3ds = env('MIDTRANS_IS_3DS');
+    }
+
     /**
      * Display a listing of the resource.
      */
@@ -55,6 +68,7 @@ class CheckoutController extends Controller
 
         // create checkout
         $checkout = Checkout::create($data);
+        $this->getSnapRedirect($checkout);
 
         // send email
         Mail::to(Auth::user()->email)->send(new AfterCheckout($checkout));
@@ -98,5 +112,64 @@ class CheckoutController extends Controller
     {
         return view('checkout.success');
     }
+
+    /**
+     * Midtrans Handler
+     */
+
+     public function getSnapRedirect(Checkout $checkout)
+     {
+        $orderId = $checkout->id.'-'.Str::random(5);
+        $price = $checkout->Camp->price * 1000;
+        $checkout->midtrans_booking = $orderId;
+        
+        $transaction_detail = [
+            "order_id" => $orderId,
+            "gross_amount" => $price
+        ];
+
+        $item_details[] = [
+            "id" => $orderId,
+            "price" => $price,
+            "quantity" => 1,
+            "name" => "Payment for {$checkout->Camp->title} Camp"
+        ];
+
+        $userData = [
+            "first_name" => $checkout->User->name,
+            "last_name" => "",
+            "address" => $checkout->User->address,
+            "city" => "",
+            "postal_code" => "",
+            "phone" => $checkout->User->phone,
+            "country_code" => "IDN",
+        ];
+
+        $customer_detail = [
+            "first_name" => $checkout->User->name,
+            "last_name" => "",
+            "email" => $checkout->User->email,
+            "phone" => $checkout->User->phone,
+            "billing_address" => $userData,
+            "shipping_address" => $userData
+        ];
+
+        $midtrans_param = [
+            "transaction_detail" => $transaction_detail,
+            "item_details" => $item_details,
+            "customer_detail" => $customer_detail
+        ];
+
+        try {
+            //Get Snap Payment Page
+            $paymentUrl = \Midtrans\Snap::createTransaction($params)->redirect_url();
+            $checkout->midtrans_url = $paymentUrl;
+            $checkout->save();
+
+            return $paymentUrl;
+        } catch (Exception $e) {
+            return false;
+        }
+     }
 
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -25,6 +25,8 @@ class User extends Authenticatable
         'password',
         'avatar',
         'occupation',
+        'phone',
+        'address',
         'is_admin'
     ];
 
